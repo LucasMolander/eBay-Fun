9971c8eee180f008eeb2ded02e3646f04d13ee20
Lucas Molander <lmmoland@ncsu.edu>
2017-12-30 22:02:56 -0500

    Multiprocessing is giving an error when I think it shouldn't be. About to move over to actual threads via the threading package (see test.py for a small example).

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ General notes:
     - How to identify a card in MTG:
         * Name and Set
 
-    - Determinants of price (for classiciation):
+    - Determinants of price:
         * Foil
         * Grade
         * Condition (might only be in item description)
@@ -25,6 +25,10 @@ General notes:
 Removes all non-ascii (less than 128) characters from a string.
 """
 def removeNonASCII(str):
+
+    print('removeNonASCII')
+    sys.stdout.flush()
+
     out = []
 
     for c in str:
@@ -33,6 +37,10 @@ def removeNonASCII(str):
     return ''.join(out)
 
 def removeOffersTaken(titles, prices):
+
+    print('removeOffersTaken')
+    sys.stdout.flush()
+
     i = len(prices) - 1
 
     while i >= 0:
@@ -41,8 +49,51 @@ def removeOffersTaken(titles, prices):
             del titles[i]
         i = i - 1
 
+"""
+Removes results containing any of the given words.
+This is case in-sensitive, for both the title and words.
+"""
+def removeContaining(titles, prices, words):
+
+    print('pricesToNumbers')
+    sys.stdout.flush()
+
+    i = len(prices) - 1
+
+    while i >= 0:
+        for w in words:
+            if w.lower() in titles[i].lower():
+                del prices[i]
+                del titles[i]
+                break
+
+        i = i - 1
+
+"""
+Removes results not containing all of the given words.
+This is case in-sensitive, for both the title and words.
+"""
+def removeNotContaining(titles, prices, words):
+
+    print('pricesToNumbers')
+    sys.stdout.flush()
+
+    i = len(prices) - 1
+
+    while i >= 0:
+        for w in words:
+            if not w.lower() in titles[i].lower():
+                del prices[i]
+                del titles[i]
+                break
+
+        i = i - 1
 
 def pricesToNumbers(prices):
+
+    print('pricesToNumbers')
+    sys.stdout.flush()
+
     out = []
 
     for price in prices:
@@ -50,7 +101,19 @@ def pricesToNumbers(prices):
 
     return out
 
-def getMedian(card):
+"""
+If it's not a foil, put ' -foil' in the card name.
+If it IS a foil,     put  ' foil'  in the card name.
+"""
+def getMedian(args):
+
+    print('getMedian')
+    sys.stdout.flush()
+
+    card     = args[0]
+    isFoil   = args[1]
+    removes  = args[2]
+
     #
     # Perform a GET request for the web page.
     #
@@ -58,9 +121,11 @@ def getMedian(card):
     searchURL += '?&LH_Complete=1&LH_Sold=1'      # Sold auctions only
     searchURL += '&_nkw='                         # Put search string here
 
-    searchString = card
-    searchString += ' -foil -4x -x4 -3x -x3 -2x -x2' # Guards
+    searchString  = card
+    searchString += (' foil' if isFoil else ' -foil')
+    searchString += ' -4x -x4 -3x -x3 -2x -x2'    # Guards
     searchString = re.sub(r' ', '%20', searchString)
+    searchString = re.sub(r"'", '%27', searchString)
 
     finalURL = searchURL + searchString
 
@@ -86,12 +151,28 @@ def getMedian(card):
     # Don't want to consider offers that were taken. Need a definite price.
     removeOffersTaken(titles, prices)
 
+    # If searching for a foil, remove cards that don't have 'foil' in the title.
+    # If searching for a non-foil, remove titles that do have 'foil'.
+    # removeOppositeFoility(titles, prices, isFoil)
+    if isFoil:
+        removeNotContaining(titles, prices, ['foil'])
+    else:
+        removeContaining(titles, prices, ['foil'])
+
+    # Extra, more specific strings to be careful of in the title.
+    # For example, we want to avoid '2017' for Modern Masters 2013.
+    removeContaining(titles, prices, removes)
+
+    # print('\n\n\n\n')
+    # print('FOIL' if isFoil else 'NOT FOIL')
+    # print('REMAINING TITLES:')
+    # for t in titles:
+    #     print(t)
+
     # Turn them into actual numerics.
     priceNumbers = pricesToNumbers(prices)
     # for priceN in priceNumbers:
     #     print(priceN)
-
-    # Just take the median for now
 
 
 
@@ -100,8 +181,9 @@ def getMedian(card):
     #
     # Try just taking the median of the five most recently-finished auctions.
     #
-    # if (len(priceNumbers) > 5):
-    #     priceNumbers = priceNumbers[0:5]
+    medianCutoff = 10
+    if (len(priceNumbers) > medianCutoff):
+        priceNumbers = priceNumbers[0:medianCutoff]
 
 
     if (len(priceNumbers) > 0):
@@ -109,7 +191,13 @@ def getMedian(card):
     else:
         return 0
 
-def reportExpansion(pathToFile):
+def reportExpansion(folder, file):
+
+    print('reportExpansion')
+    sys.stdout.flush()
+
+    pathToFile = folder + '/' + file
+
     with open(pathToFile, 'r') as inFile:
         rawContents = inFile.read().split('\n')
 
@@ -118,9 +206,6 @@ def reportExpansion(pathToFile):
     pRare     = 7.0 / 8.0
     pUncommon = 3.0
     pCommon   = 10.0
-
-    # Let's just let the foil multiplier be 2 for now
-    foilMultiplier = 2.0
 
     # Get some basic info from the top of the file
     setName = rawContents[0]
@@ -153,7 +238,7 @@ def reportExpansion(pathToFile):
     rareNames = []
     uncommonNames = []
     commonNames = []
-    allNames = []      # Work for multithreading
+    allNames = [] # Nice for multithreading
 
     for i in range(mStart, mEnd + 1):
         mythicNames.append(rawContents[i])
@@ -171,47 +256,116 @@ def reportExpansion(pathToFile):
         commonNames.append(rawContents[i])
         allNames.append(rawContents[i])
 
+    # These lists are fed to the function during multithreading.
+    # Need to search for both foil and non-foil cards.
+    allNamesNonFoil = []
+    allNamesFoil = []
+
+    # Set-specific 'removes' it to be extra careful of
+    if 'Modern Masters 2013' in setName:
+        removes = ['2015', '2017', 'MM15', 'MM17']
+    elif 'Modern Masters 2015' in setName:
+        removes = ['2013', '2017', 'MM13', 'MM17']
+    elif 'Modern Masters 2017' in setName:
+        removes = ['2013', '2015', 'MM13', 'MM15']
+    else:
+        removes = []
+
+    for n in allNames:
+        allNamesNonFoil.append([n, False, removes])
+        allNamesFoil.append([n, True, removes])
+
+
+
+    #
+    # TESTING
+    #
+    # Using a single thread for debugging purposes.
+    #
+    # allPrices = []
+    # allPricesFoil = []
+    # for nnf in allNamesNonFoil:
+    #     allPrices.append(getMedian(nnf))
+    # for nf in allNamesFoil:
+    #     allPricesFoil.append(getMedian(nf))
+
+
     # Figure out the prices
     # In a multithreaded way, of course. See the README for details.
     pool = ThreadPool(8)
-    allPrices = pool.map(getMedian, allNames)
+    allPrices = pool.map(getMedian, allNamesNonFoil)
+    # Make sure to also get all foil prices, to calculate the ev of a foil.
+    allPricesFoil = pool.map(getMedian, allNamesFoil)
+
+    if sum(allPrices) > 0.0:
+        foilMultiplier = (sum(allPricesFoil) / sum(allPrices))
+    else:
+        foilMultiplier = 0.0
 
     # Put the prices back into their categories.
     cur = 0
     nxt = len(mythicNames)
     mythicPrices = allPrices[cur:nxt]
 
+    # TEMPORARY
+    mythicPricesFoil = allPricesFoil[cur:nxt]
+
     cur  = nxt
     nxt += len(rareNames)
     rarePrices = allPrices[cur:nxt]
+
+    # TEMPORARY
+    rarePricesFoil = allPricesFoil[cur:nxt]
 
     cur  = nxt
     nxt += len(uncommonNames)
     uncommonPrices = allPrices[cur:nxt]
 
+    # TEMPORARY
+    uncommonPricesFoil = allPricesFoil[cur:nxt]
+
     cur  = nxt
     nxt += len(commonNames)
     commonPrices = allPrices[cur:nxt]
+
+    # TEMPORARY
+    commonPricesFoil = allPricesFoil[cur:nxt]
 
     # Print them out
     print('Mythic prices:')
     for i in range(0, len(mythicNames)):
         print(str(round(mythicPrices[i], 2)) + '\t' + mythicNames[i])
+
+        # TEMPORARY
+        print(str(round(mythicPricesFoil[i], 2)) + '\t' + mythicNames[i] + '\n')
+
         sys.stdout.flush()
 
     print('\n\nRare prices:')
     for i in range(0, len(rareNames)):
         print(str(round(rarePrices[i], 2)) + '\t' + rareNames[i])
+
+        # TEMPORARY
+        print(str(round(rarePricesFoil[i], 2)) + '\t' + rareNames[i] + '\n')
+
         sys.stdout.flush()
 
     print('\n\nUncommon prices:')
     for i in range(0, len(uncommonNames)):
         print(str(round(uncommonPrices[i], 2)) + '\t' + uncommonNames[i])
+
+        # TEMPORARY
+        print(str(round(uncommonPricesFoil[i], 2)) + '\t' + uncommonNames[i] + '\n')
+
         sys.stdout.flush()
 
     print('\n\nCommon prices:')
     for i in range(0, len(commonNames)):
         print(str(round(commonPrices[i], 2)) + '\t' + commonNames[i])
+
+        # TEMPORARY
+        print(str(round(commonPricesFoil[i], 2)) + '\t' + commonNames[i] + '\n')
+
         sys.stdout.flush()
 
     # Averages
@@ -221,10 +375,10 @@ def reportExpansion(pathToFile):
     uncommonTotal = sum(commonPrices)
     allTotal      = mythicTotal + rareTotal + commonTotal + uncommonTotal
 
-    evMythic   = mythicTotal   / float(nMythics)
-    evRare     = rareTotal     / float(nRares)
-    evUncommon = commonTotal   / float(nUncommons)
-    evCommon   = uncommonTotal / float(nCommons)
+    evMythic   = (mythicTotal   / float(nMythics))   if nMythics   > 0 else 0.0
+    evRare     = (rareTotal     / float(nRares))     if nRares     > 0 else 0.0
+    evUncommon = (commonTotal   / float(nUncommons)) if nUncommons > 0 else 0.0
+    evCommon   = (uncommonTotal / float(nCommons))   if nCommons   > 0 else 0.0
 
     print('\n\n')
     print('Expected Mythic price:   ' + str(round(evMythic, 2)))
@@ -236,6 +390,7 @@ def reportExpansion(pathToFile):
     evFoil = evRandom * foilMultiplier
 
     print('Expected foil price:     ' + str(round(evFoil, 2)))
+    print('\tFoil multiplier = ' + str(round(foilMultiplier, 2)))
 
     print('\nMythics add:   ' + str(round((evMythic   * pMythic),   2)))
     print(  'Rares add:     ' + str(round((evRare     * pRare),     2)))
@@ -263,4 +418,4 @@ import re
 import statistics
 from multiprocessing.dummy import Pool as ThreadPool
 
-reportExpansion('Expansions/Eternal Masters.txt')
+reportExpansion('Expansions', 'Iconic Masters.txt')
--- test.py
+++ test.py
@@ -3,10 +3,78 @@ This file contains short tests for sanity checks.
 """
 
 
+def myFun(index, nums, out):
+	print(str(index) + ': ' + str(sum(nums)))
+
+	# This should be okay because each thread gets a unique index.
+	out[index] = sum(nums)
+	return
 
 
-myArr = [1, 2, 3, 4, 5]
-print(myArr[0:2])
+import threading
+
+# Create the threads
+threads = []
+output = []
+for i in range(0,5):
+	output.append(0)
+	t = threading.Thread(target=myFun, args=(i, [i, i + 1], output))
+	threads.append(t)
+	t.start()
+
+# # Run the threads
+# for t in threads:
+# 	t.start()
+
+for t in threads:
+	t.join()
+
+for o in output:
+	print(o)
+
+print('Done!')
+
+
+# import re
+
+# foilTitle = 'this is a string that contains Foil'
+# nonFoilTitle = 'MTG 1X The Locust God X1 Hour of Devastation NM/MT Magic HOU Legendary'
+
+# boolIsTrue  = True
+# boolIsFalse = False
+
+# print('boolIsTrue is ' + ('TRUE' if boolIsTrue else 'FALSE'))
+# print('boolIsFalse is ' + ('TRUE' if boolIsFalse else 'FALSE'))
+
+# if (re.search(r'foil', foilTitle, re.IGNORECASE) != None):
+# 	print('Search for \"foil\" succeeded in foilTitle')
+# else:
+# 	print('Search for \"foil\" failed in foilTitle')
+
+# if (re.search(r'foil', nonFoilTitle, re.IGNORECASE) != None):
+# 	print('Search for \"foil\" succeeded in nonFoilTitle')
+# else:
+# 	print('Search for \"foil\" failed in nonFoilTitle')
+
+
+
+# if (re.search(r'foil', foilTitle, re.IGNORECASE) == None):
+# 	print('Search for \"foil\" negated in foilTitle')
+# else:
+# 	print('Search for \"foil\" did not negate in foilTitle')
+
+# if (re.search(r'foil', nonFoilTitle, re.IGNORECASE) == None):
+# 	print('Search for \"foil\" negated in nonFoilTitle')
+# else:
+# 	print('Search for \"foil\" did not negate in nonFoilTitle')
+
+
+
+
+
+
+# myArr = [1, 2, 3, 4, 5]
+# print(myArr[0:2])
 
 
 
